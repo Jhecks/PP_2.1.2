3c9f094e2e3925a1e9bf5935fdc7acf68ebc70ce
Michael <semenov.yuri.michael@gmail.com>
2022-04-21 16:47:20 +0300

    Cat

--- src/main/java/Cat.java
+++ src/main/java/Cat.java
@@ -1,2 +1,12 @@
 public class Cat {
+    private String name;
+    private String speech;
+
+    public Cat(String name) {
+        this.name = name;
+    }
+
+    public void setSpeech(String speech) {
+        this.speech = speech;
+    }
 }
